58fcbe8e56417d7a8773272e503d5c8570099016
Marco <marco.villegas@live.se>
2023-03-25 15:49:28 +0100

    fix responsive CSS

--- CodeRed/src/app/components/ai-chat/ai-chat.component.ts
+++ CodeRed/src/app/components/ai-chat/ai-chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,60 +9,72 @@ import { HttpClient } from '@angular/common/http';
 export class AiChatComponent {
   messages: any[] = [];
   newMessage: string = '';
-  @ViewChild('chatContainer', {static: false}) private chatContainer: ElementRef | null = null;
-  @ViewChild('submitContainer', {static: false}) private submitContainer: ElementRef | null = null;
+  @ViewChild('chatContainer', { static: false }) private chatContainer: ElementRef | null = null;
 
-  constructor(private http: HttpClient,private renderer: Renderer2) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     console.info('Marco test');
 
     this.messages = [
-      { sender: 'Bot', content: 'Hi there! How can I help you?' },
+      { sender: 'Ava', content: 'Hi there! How can I help you?' },
       { sender: 'User', content: 'Can you tell me more about your product?' },
-      { sender: 'Bot', content: 'Sure! Our product is designed to...' },
+      { sender: 'Ava', content: 'Sure! Our product is designed to...' },
       { sender: 'User', content: 'That sounds great. How do I get started?' },
-      { sender: 'Bot', content: 'You can get started by...' },
-      { sender: 'Bot', content: 'Hi there! How can I help you?' },
+      { sender: 'Ava', content: 'You can get started by...' },
+      { sender: 'Ava', content: 'Hi there! How can I help you?' },
       { sender: 'User', content: 'Can you tell me more about your product?' },
-      { sender: 'Bot', content: 'Sure! Our product is designed to...' },
+      { sender: 'Ava', content: 'Sure! Our product is designed to...' },
       { sender: 'User', content: 'That sounds great. How do I get started?' },
-      { sender: 'Bot', content: 'You can get started by...' },
-      { sender: 'Bot', content: 'Hi there! How can I help you?' },
+      { sender: 'Ava', content: 'You can get started by...' },
+      { sender: 'Ava', content: 'Hi there! How can I help you?' },
       { sender: 'User', content: 'Can you tell me more about your product?' },
-      { sender: 'Bot', content: 'Sure! Our product is designed to...' },
+      { sender: 'Ava', content: 'Sure! Our product is designed to...' },
       { sender: 'User', content: 'That sounds great. How do I get started?' },
-      { sender: 'Bot', content: 'You can get started by...' },
+      { sender: 'Ava', content: 'You can get started by...' },
     ];
 
     this.exampleAnswers();
   }
 
   ngAfterViewInit() {
-    // scroll to bottom of chat container
     if (this.chatContainer) {
       this.scrollToLastMessage()
     }
   }
 
   scrollToLastMessage(): void {
- 
-    if (this.chatContainer) {
-      const element = this.chatContainer.nativeElement;
-      const lastMessage = element.lastElementChild;
-      if (lastMessage) {
-        lastMessage.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-      }
+    
+    setTimeout(() => {
+
+    const element = document.getElementById("advanced-tools"); // Your target element
+    const headerOffset = 45;
+
+    if (element) {
+      const elementPosition = element.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
     }
- 
+    /* const lastMessageIndex = this.messages.length - 1;
+    const lastMessageId = 'message-' + lastMessageIndex;
+    const lastMessageElement = document.getElementById('advanced-tools');
+    if (lastMessageElement) {
+      lastMessageElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    } */
+  },300);
 
   }
 
- 
+
 
   exampleAnswers() {
     this.messages.push({
-      sender: 'Bot',
+      sender: 'Ava',
       content: 'Hello! How can I help you today?',
     });
     this.messages.push({
@@ -70,7 +82,7 @@ export class AiChatComponent {
       content: 'Can you tell me about the weather in New York?',
     });
     this.messages.push({
-      sender: 'Bot',
+      sender: 'Ava',
       content:
         'Sure! The weather in New York is currently 70 degrees and sunny.',
     });
@@ -79,18 +91,18 @@ export class AiChatComponent {
       content: 'What is the capital of France?',
     });
     this.messages.push({
-      sender: 'Bot',
+      sender: 'Ava',
       content: 'The capital of France is Paris.',
     });
     this.messages.push({ sender: 'User', content: 'Thanks for your help!' });
     this.messages.push({
-      sender: 'Bot',
+      sender: 'Ava',
       content: 'Youre welcome. Have a great day!',
     });
   }
 
   onSubmit() {
-    if (this.newMessage) {
+    /*if (this.newMessage) {
       this.messages.push({ sender: 'User', content: this.newMessage });
       this.http
         .post(
@@ -114,6 +126,18 @@ export class AiChatComponent {
           this.messages.push({ sender: 'Bot', content: botMessage });
         });
       this.newMessage = '';
-    }
+    } */
+
+    this.messages.push({
+      sender: 'Ava',
+      content: 'Youre welcome. Have a great day!',
+    });
+
+ 
+
+      this.scrollToLastMessage();
+
+
   }
+
 }
