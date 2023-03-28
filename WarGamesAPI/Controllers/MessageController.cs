e7d0958bfc6aa09dfb05424fe387416aef8e11ff
Peter L <peter.lindholm@iths.se>
2023-03-28 08:55:47 +0200

    Add Logger

---
+++ WarGamesAPI/Controllers/MessageController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarGamesAPI.Model;
+
+namespace WarGamesAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MessageController : ControllerBase
+{
+    // GET: api/<ValuesController>
+    [HttpGet]
+    public IEnumerable<Message> GetMessages()
+    {
+        var result = new List<Message>
+        {
+            new() { Id = 1, Question = "What is 2 + 2?", Answer = "5" },
+            new() { Id = 2, Question = "Is the Easter Bunny real?", Answer = "Yes" }
+        };
+
+        return result;
+    }
+
+    // GET api/<ValuesController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<ValuesController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<ValuesController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<ValuesController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
--- WarGamesAPI/Program.cs
+++ WarGamesAPI/Program.cs
@@ -1,11 +1,28 @@
+
+using Serilog;
+using Serilog.Events;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Debug()
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
