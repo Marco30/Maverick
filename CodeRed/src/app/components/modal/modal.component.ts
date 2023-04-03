9542b21a997733dd20e10abe69b8515ceed1f245
khaled85abo <khaledabou1985@gmail.com>
2023-04-04 00:23:19 +0200

    Change loader, modal to satndalone component

--- CodeRed/src/app/components/modal/modal.component.ts
+++ CodeRed/src/app/components/modal/modal.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Output, Input, EventEmitter } from "@angular/core";
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: "app-modal",
-  templateUrl: "./modal.component.html",
-  styleUrls: ["./modal.component.css"],
+  standalone: true,
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
   @Output() closeModal = new EventEmitter();
