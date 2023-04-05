d90a35204efece481aaf9410b6672628a8929b3f
Peter L <peter.lindholm@iths.se>
2023-04-05 08:30:05 +0200

    update RegisterUserDto

--- WarGamesAPI/DTO/RegisterUserDto.cs
+++ WarGamesAPI/DTO/RegisterUserDto.cs
@@ -1,17 +1,23 @@
+
 namespace WarGamesAPI.DTO;
 
 public class RegisterUserDto
 {
-    public string? SocialSecurityNumber { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? FullName { get; set; }
+    public string? SocialSecurityNumber { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
-    public string? PhoneNumber { get; set; }
-    public string? Gender { get; set; }
-    public int? AddressId { get; set; }
+    public bool AgreeMarketing { get; set; }
+    public bool SubscribeToEmailNotification { get; set; }
+    public Genders Gender { get; set; }
     public AddressDto? Address { get; set; }
+    public int? PhoneNumber { get; set; }
+    public int? MobilePhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
 
+    public int? AddressId { get; set; }
 
 
 
